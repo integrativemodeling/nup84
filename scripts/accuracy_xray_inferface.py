5e2e53bd95b125eea834115884d244b3c652aab3
Ben Webb <ben@salilab.org>
2014-11-03 16:29:57 -0800

    Fix arguments.

--- scripts/accuracy_xray_inferface.py
+++ scripts/accuracy_xray_inferface.py
@@ -7,9 +7,9 @@ import glob
 
 rmfs=glob.glob("output.1/rmfs/*.rmf3")
 
-selection_dictionary={"Nup145c-Sec13":[("Nup145c",145,181),"Sec13"],
-                      "Nup85-Seh1":[("Nup85",123,460),"Seh1"],
-                      "Nup84-Nup145c":[("Nup84",1,488),("Nup145c",145,181)]}
+selection_dictionary={"Nup145c-Sec13":[(145,181,"Nup145c"),"Sec13"],
+                      "Nup85-Seh1":[(123,460,"Nup85"),"Seh1"],
+                      "Nup84-Nup145c":[(1,488,"Nup84"),(145,181,"Nup145c")]}
 
 model=IMP.Model()
 
@@ -21,11 +21,11 @@ pr=IMP.pmi.analysis.Precision(model,'one',
                               selection_dictionary=selection_dictionary)
 pr.set_precision_style('pairwise_drmsd_k')
 
-pr.add_structures(zip(rmfs,frames),is_mpi=False)
+pr.add_structures(zip(rmfs,frames), 'all', is_mpi=False)
 
 
 
 refrmf='reference/xray-hub.rmf3'
 pr.set_reference_structure(refrmf,0)
 
-print pr.get_average_distance_wrt_reference_structure()
+print pr.get_average_distance_wrt_reference_structure('all')
