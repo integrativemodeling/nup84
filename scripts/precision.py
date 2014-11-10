0ac4fe95f9f418bd9aad88562eecd779e70d240d
Ben Webb <ben@salilab.org>
2014-11-10 15:24:05 -0800

    The 'all' precision is always calculated.

--- scripts/precision.py
+++ scripts/precision.py
@@ -7,23 +7,17 @@ import glob
 
 selection_dictionary={"Hub":["Nup145c","Sec13","Seh1","Nup85"]}
 
+rmfs = glob.glob("kmeans_50_1/cluster.0/*.rmf3")
+frames=[0]*len(rmfs)
 
-for eoe in ('hub', 'all'):
-    rmfs = glob.glob("kmeans_50_1/cluster.0/*.rmf3")
-    frames=[0]*len(rmfs)
-    
-    model=IMP.Model()
-    
-    if eoe=='all':
-       pr=IMP.pmi.analysis.Precision(model,'ten',
-                                     selection_dictionary={})
-    else:
-       pr=IMP.pmi.analysis.Precision(model,'ten',
-                                     selection_dictionary=selection_dictionary)
-    pr.set_threshold(60.0)
-    pr.set_precision_style('pairwise_drmsd_Q')
+model=IMP.Model()
 
-    pr.add_structures(zip(rmfs,frames), 'all', is_mpi=False)
-        
-    outfile="precision."+eoe+".dat"
-    pr.get_precision(outfile, 'all', 'all', is_mpi=False, skip=1)
+pr=IMP.pmi.analysis.Precision(model,'ten',
+                             selection_dictionary=selection_dictionary)
+pr.set_threshold(60.0)
+pr.set_precision_style('pairwise_drmsd_Q')
+
+pr.add_structures(zip(rmfs,frames), 'all', is_mpi=False)
+
+outfile="precision.dat"
+pr.get_precision(outfile, 'all', 'all', is_mpi=False, skip=1)
