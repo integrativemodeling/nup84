4c9047ec54c019b9520053bf8463b1ebddd02606
Ben Webb <ben@salilab.org>
2017-03-27 12:06:00 -0700

    Use XL_analysis git repository.
    Rather than using a local copy of the XL_analysis
    scripts and making our own changes, push the changes upstream
    to the XL_analysis repository on GitHub and then include a
    copy of that repository (with git subtree) here.

--- scripts/contact_map/make_contact_map.py
+++ scripts/contact_map/make_contact_map.py
@@ -1,6 +1,9 @@
 import os
 import matplotlib
 matplotlib.use('Agg')
+import sys
+sys.path.insert(0, os.path.join(os.path.dirname(sys.argv[0]),
+                                'XL_analysis', 'src'))
 import xltable
 import glob
 
